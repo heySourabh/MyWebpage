b31205b49729fe3f2bc6e652bf85bc64dffd93ce
Sourabh Bhat <heySourabh@gmail.com>
2019-01-24 14:38:15 +0530

    added chapter 4

---
+++ cff/create_table.py
@@ -0,0 +1,10 @@
+questions = 32
+for q in range(1, questions + 1):
+    print('<tr>')
+    print('<th>%d</th>' % q)
+    print('<td><img src="images/q%02d.png" alt=""><br>' % q)
+    print('problem desc')
+    print('</td>')
+    print('<td><a href="#">q%02d.pdf</a></td>' % q)
+    print('<td><a href="#">q%02d.py</a></td>' % q)
+    print('</tr>')

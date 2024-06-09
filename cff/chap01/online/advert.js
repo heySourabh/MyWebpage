3146aa0f0bbaa1f5c8bfc99cf7993d788011fd32
Sourabh Bhat <heySourabh@gmail.com>
2024-06-09 13:24:41 +0200

    update adsense

--- cff/chap01/online/advert.js
+++ cff/chap01/online/advert.js
@@ -7,11 +7,18 @@ for (let i = 0; i < adElems.length; i++) {
     <p>Content continues after advertisement...</p>
     <div style="background:lightblue; padding:1em; margin:0;">
     <p>--- Ad ---</p>
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5232558571567163"
-            data-ad-slot="5414144171" data-ad-format="auto" data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5232558571567163"
+    crossorigin="anonymous"></script>
+    <!-- CFF inline ads -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-5232558571567163"
+        data-ad-slot="2481369001"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
     ---
     </div>
     <p>... Content continues ...</p>

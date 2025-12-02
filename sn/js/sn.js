6c94cf3e8f53541fbf650bdd16e1747ed6386752
Sourabh Bhat <heySourabh@gmail.com>
2025-12-02 11:54:01 +0100

    Updated sn comment

--- sn/js/sn.js
+++ sn/js/sn.js
@@ -370,11 +370,6 @@ function msToTime(duration) {
 }
 
 function savePrefs() {
-    var exdays = 10;
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-
     var totalSN = document.getElementById("totalSN").value;
     var delayBeforeStart = document.getElementById("delay").value;
     var speed = document.getElementById("speed_range").value;
@@ -414,9 +409,9 @@ function loadPrefs() {
     }
 }
 
-function setCookie(cname, cvalue, exdays = 10) {
+function setCookie(cname, cvalue, cookie_expiry_days = 10) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + (cookie_expiry_days * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }

0389832d54ba84e8648341ea386027a15733effe
Sourabh Bhat <heySourabh@gmail.com>
2026-02-07 12:32:48 +0100

    share sn app

---
+++ sn/app/share.js
@@ -0,0 +1,19 @@
+const shareData = {
+    title: "Suryanamaskar App",
+    text: "Practice daily Suryanamaskar & stay fit!",
+    url: "https://spbhat.in/sn/",
+};
+
+const shareBtn = document.getElementById("share-btn");
+if (!navigator.canShare) {
+    shareBtn.style.display = "none";
+}
+
+shareBtn.addEventListener("click", async () => {
+    try {
+        await navigator.share(shareData);
+    } catch (err) {
+        console.log(err);
+    }
+    shareBtn.style.display = "none";
+});

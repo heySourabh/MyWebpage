6f90a3e91887455c2074b93e91211ae2b73674f9
Sourabh Bhat <heySourabh@gmail.com>
2026-03-29 12:28:52 +0200

    Updated figure 1 problem table

--- cff/create_table.py
+++ cff/create_table.py
@@ -1,10 +1,45 @@
-questions = 48
-for q in range(1, questions + 1):
+description = [
+    "Is the flow incompressible?",
+    "Gravitational force acting on a spacecraft",
+    "Units conversion",
+    "Isentropic process",
+    "Conservation of energy",
+    "Conservation of mass",
+    "Thrust developed by jet engine",
+    "Jet efflux velocity",
+    "Thrust with afterburner",
+    "Thrust developed by rocket",
+    "Effect of ambient pressure on thrust",
+    "Mass flow rate of hydrogen",
+    "Jet propulsion system for an automobile",
+    "Flow of CO<sub>2</sub> with heat addition",
+    "Heat exchanger",
+    "Force acting on nozzle of rocket engine"
+]
+
+print('<table class="table table-hover">')
+print('<thead class="thead-light">')
+print('<tr>')
+print('<th>#</th>')
+print('<th>Problem Description</th>')
+print('<th>Online</th>')
+print('</tr>')
+print('</thead>')
+
+print('<tbody>')
+
+for i, desc in enumerate(description):
+    q = i+1
     print('<tr>')
-    print('<th>%d</th>' % q)
-    print('<td><img src="images/q%02d.png" alt=""><br>' % q)
-    print('problem desc')
+    print(f'<th>{q}</th>')
+    print('<td>')
+    print(f'<a href="online/{q:02d}/index.html">')
+    print(f'<img src="images/{q:02d}.png" width="200" alt="Problem {q}"><br>')
+    print(desc)
+    print('</a>')
     print('</td>')
-    print('<td><a href="#">q%02d.pdf</a></td>' % q)
-    print('<td><a href="#">q%02d.py</a></td>' % q)
+    print(f'<td><a href="online/{q:02d}/index.html">link</a></td>')
     print('</tr>')
+
+print('</tbody>')
+print('</table>')
